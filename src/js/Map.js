c6caba9b085c5287881b9090cbec814407a6a1e8
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-04-08 15:40:19 +0900

    Fix map rendering issue on IOS device

--- src/js/Map.js
+++ src/js/Map.js
@@ -15,7 +15,8 @@ class Map {
   static cachedLibraries = new globalThis.Map();
   // Called once on App.init()
   static initMarkerHandler(row) {
-    Map.container.style.zIndex = '399';
+    // IOS
+    Map.driver.style.zIndex = '399';
     Map.container.addEventListener('click', function (e) {
       const selectedLibraryData = Sidebar.selectedLibraryData(e, row);
       if (!selectedLibraryData) return;
